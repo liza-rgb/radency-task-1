3035769cca7f4a8b2efaba2be4aef6a76ab2eb17
liza-rgb <68602250+liza-rgb@users.noreply.github.com>
2022-09-13 00:17:46 +0200

    Add seeds data

---
+++ src/js/seeds.js
@@ -0,0 +1,40 @@
+export const seeds = [
+    {
+        name: "Shopping list",
+        category: "Task",
+        content: "Tomatoes, bread"
+    },
+    {
+        name: "The theory of evolution",
+        category: "Random Thought",
+        content: "Theory in biology postulating that the various types of plants, animals...",
+    },
+    {
+        name: "New Feature",
+        category: "Idea",
+        content: "I'm gonna have a dentist appointment on the 3/5/2021, I moved it from 5/5/2021"   
+    },
+    {
+        name: "William Gaddis", 
+        category: "Quote",
+        content: "Justice? -You get justice in the next world, in this world you have the law."
+    }, 
+    {
+        name: "New Feature",
+        created: new Date(),
+        category: "Idea",
+        content: "I'm gonna have a dentist appointment on the 3-5-2021, I moved it from 5-5-2021"   
+    }, 
+    {
+        name: "Pizza recipe",
+        created: new Date(),
+        category: "Idea",
+        content: "I had a dream about this pizza. Must try to cook it!"   
+    }, 
+    {
+        name: "Lab #2 Algorithms and Data Structures",
+        created: new Date(),
+        category: "Task",
+        content: "It is due on 15/09/2022"   
+    }
+];
